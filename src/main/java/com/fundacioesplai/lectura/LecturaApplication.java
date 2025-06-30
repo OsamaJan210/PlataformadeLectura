a5f108cadf349191df2220b255dbd08d6063a886
Muhammad Osama Jan <osamajan210@gmail.com>
2025-06-30 12:02:46 +0200

    first commit

---
+++ src/main/java/com/fundacioesplai/lectura/LecturaApplication.java
@@ -0,0 +1,13 @@
+package com.fundacioesplai.lectura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundacioesplai/lectura/LecturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundacioesplai.lectura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
